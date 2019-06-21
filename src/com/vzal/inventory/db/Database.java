9770af89da0358c78effd6110c24a1ab1a2a8a1d
LuisReyes98 <luisrogelio98@gmail.com>
2019-06-21 18:32:11 -0400

    initial commit

---
+++ src/com/vzal/inventory/db/Database.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vzal.inventory.db;
+
+/**
+ *
+ * @author luisr
+ */
+public class Database implements DatabaseInterface {
+
+    public static String buildCreateTable() {
+
+    }
+}
---
+++ src/com/vzal/inventory/db/DatabaseAbstractModel.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vzal.inventory.db;
+
+/**
+ *
+ * @author luisr
+ */
+public abstract class DatabaseAbstractModel {
+
+    public abstract void read();
+
+    public abstract void write();
+
+    public abstract void edit();
+
+    public abstract void delete();
+
+    public abstract void install();
+
+}
