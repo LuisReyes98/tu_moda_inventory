6734c9d1f7aa03ae96391722b1d1ac71c7932d90
LuisReyes98 <luisrogelio98@gmail.com>
2019-07-13 00:14:08 -0400

    safely delete of categories

--- src/com/vzla/inventory/products/controller/CategoriesController.java
+++ src/com/vzla/inventory/products/controller/CategoriesController.java
@@ -1,10 +1,15 @@
 package com.vzla.inventory.products.controller;
 
+import com.j256.ormlite.dao.GenericRawResults;
+import com.j256.ormlite.stmt.PreparedQuery;
+import com.j256.ormlite.stmt.QueryBuilder;
 import com.vzla.inventory.controller.MainController;
 import com.vzla.inventory.controller.NavigationController;
 import com.vzla.inventory.main.Main;
 import com.vzla.inventory.products.models.Category;
+import com.vzla.inventory.products.models.Product;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,9 +30,44 @@ public class CategoriesController extends MainController {
             this.categories = Main.db.categoryDao.queryForAll().toArray();
         } catch (SQLException ex) {
             Logger.getLogger(ProductsController.class.getName()).log(Level.SEVERE, null, ex);
-            System.out.println("Error ProductsController getCategories " + ex.getMessage());
+            System.out.println("Error CategoriesController viewCategories " + ex.getMessage());
         }
         NavigationController.goToView("categories_index", true, true);
 
+    }
+
+    /**
+     * Safe Delete of a category
+     *
+     * @param id
+     * @return
+     */
+    public boolean deleteCategory(int id) {
+        GenericRawResults<String[]> rawResults;
+        List<String[]> results;
+        String[] resultArray;
+        try {
+            rawResults
+                    = Main.db.productDao.queryRaw(
+                            String.format(
+                                    "select count(*) from product where %s = %d",
+                                    Product.CATEGORY_FIELD_NAME,
+                                    id)); //raw sql with orm with count, one value is expected
+            results = rawResults.getResults();//list of results
+            resultArray = results.get(0); // first array of the list of results
+
+            if (Integer.parseInt(resultArray[0]) >= 1) { //if the count is greater than 0 then this category cannot be deleted
+                return false;
+            } else {
+                //The category can be safely deleted
+                Main.db.categoryDao.deleteById(id);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(CategoriesController.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Error CategoriesController deleteCategory " + ex.getMessage());
+            return false; //error deleting
+        }
+        this.viewCategories(); //has been safely deleted then reload
+        return true;
     }
 }
--- src/com/vzla/inventory/products/views/categories.js
+++ src/com/vzla/inventory/products/views/categories.js
@@ -5,17 +5,32 @@ function preload() {
         el: '#tu_moda_app',
         data: {
             categories: JAVA_CONTROLLER.categories,
+            category_name: null,
+            category_id: null,
         },
         methods: {
             editCategory: function (id) {
-                JAVA_CONTROLLER.viewCategoryEdit(id);
+                // JAVA_CONTROLLER.viewCategoryEdit(id);
+                JAVA_CONTROLLER.editCategory(id, $(`#category_name_${id}`).val());
+
             },
-            deleteCategory: function (id) {
-                JAVA_CONTROLLER.deleteCategory(id);
+            deleteCategory: function (id,name) {
+                $("#generic_modal_title").html("Error")
+                $("#generic_modal_body").html(`No se pudo eliminar la categoria <strong>${name}</strong> \n ya que hay productos que pertenecen a ella`);
+
+                if (!JAVA_CONTROLLER.deleteCategory(id)) {
+                    $(`#delete${id}`).modal('hide');
+                    $("#generic_modal").modal('show');
+                }
+                // JAVA_CONTROLLER.deleteCategory(id);
             },
             goToProducts: function () {
                 JAVA_CONTROLLER.goToProducts();
-            }
+            },
+            createCategory: function () {
+
+            },
+
         },
     })
 }
